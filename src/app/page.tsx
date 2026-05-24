adb2f45f6dbab4df21adf8ade77399d3eb16628a
Sankalp <sankalp4san@gmail.com>
2026-05-24 19:58:23 +0530

    Change product grid to flex-wrap to center orphaned cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,9 +113,11 @@ export default function HomePage() {
               </p>
             </div>
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+            <div className="flex flex-wrap justify-center gap-8">
               {products.map((product) => (
-                <ProductCard key={product.id} product={product} />
+                <div key={product.id} className="w-full sm:w-[calc(50%-1.5rem)] lg:w-[calc(33.333%-1.5rem)] xl:w-[calc(25%-1.5rem)] max-w-[340px]">
+                  <ProductCard product={product} />
+                </div>
               ))}
             </div>
           )}
